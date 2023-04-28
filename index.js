00861c179b6b32b07544ed025fa7f3b17bda0ef6
Rahul Raj <rahulraj94316@gmail.com>
2023-04-28 12:41:19 +0530

    box-shadow porps added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let selectedGame = document.getElementById("game");
 let gameDivs = document.getElementById("boxNumber");
-
+let copyText = document.getElementById("copyMe");
+let tooltip = document.getElementById("tooltip");
+let shadowListVal = document.getElementsByClassName("shadowVal");
+let boxshadow = document.getElementById("boxshadow");
 /*
 gameType ---game contains the number of letter 
 */
@@ -34,75 +37,127 @@ function submit() {
     formedLetter += collectLetters[i].value;
   }
 
- let isTruncate= validate(formedLetter, gameType) 
- if(isTruncate){
-  return ;
- }
+  let isTruncate = validate(formedLetter, gameType);
+  if (isTruncate) {
+    return;
+  }
 
-  switch(+gameType) {
-    case 5 :{
-      if(formedLetter in obj5){
-        alert(`you win`)
-       }
+  switch (+gameType) {
+    case 5: {
+      if (formedLetter in obj5) {
+        alert(`you win`);
+      }
       break;
     }
-    case 6 :{
-      if(formedLetter in obj6){
-        alert(`you win`)
-       }
+    case 6: {
+      if (formedLetter in obj6) {
+        alert(`you win`);
+      }
       break;
     }
-    case 7 :{
-      if(formedLetter in obj7){
-        alert(`you win`)
-       }
+    case 7: {
+      if (formedLetter in obj7) {
+        alert(`you win`);
+      }
       break;
     }
-    case 8 :{
-      if(formedLetter in obj8){
-        alert(`you win`)
-       }
+    case 8: {
+      if (formedLetter in obj8) {
+        alert(`you win`);
+      }
       break;
     }
   }
-
-
-
-
 }
 function validate(word, len) {
-  console.log('word,len', word,word.length,+len)
+  console.log("word,len", word, word.length, +len);
   if (word.length !== +len) {
-    console.log('hi',)
+    console.log("hi");
     alert(`please fill all the divs`);
     return 1;
   }
   return 0;
 }
 
-
 /**
  * @obj5 here add all the words of size 5
  * @obj6 here add all the words of size 6
- * 
+ *
  */
 
-var obj5 ={
-  'whole':'whole',
- 
+var obj5 = {
+  whole: "whole",
+};
+
+var obj6 = {
+  wholes: "wholes",
+};
+
+var obj7 = {
+  wholess: "wholess",
+};
+
+var obj8 = {
+  wholesss: "wholesss",
+};
+
+copyText.addEventListener("click", async () => {
+  console.log("clicked", copyText.innerText);
+  try {
+    await navigator.clipboard.writeText(copyText.innerText);
+
+    tooltip.innerHTML = `
+    <p class='textCopied' id='text'>Copied Success!</p>
+    `;
+    setTimeout(() => {
+      console.log("timeer");
+      document.getElementsByClassName("textCopied")[0].style = `display:none`;
+    }, 1000);
+  } catch (err) {
+    console.log("failed to copy");
+  }
+});
+let xOffset=0, yOffset=0, bRadius=0, spreadVal=0,colorPick='red';
+function triggerShadow(para) {
+  console.log('para', para)
+  switch (para) {
+    case "x": {
+      xOffset = shadowListVal[0].value;
+      break;
+    }
+    case "y": {
+      yOffset = shadowListVal[1].value;
+      break;
+    }
+    case "b": {
+      bRadius = shadowListVal[2].value;
+      break;
+    }
+    case "s": {
+      spreadVal = shadowListVal[3].value;
+      break;
+    }
+    case "c": {
+      colorPick = shadowListVal[4].value;
+      break;
+    }
+  }
+  
+
+console.log('colorPick', colorPick)
+
+  boxshadow.style = `box-shadow:${xOffset}px ${yOffset}px  ${bRadius}px ${spreadVal}px ${colorPick}`;
 }
 
-var obj6 ={
-  'wholes':'wholes',
- 
-}
 
-var obj7 ={
-  'wholess':'wholess',
- 
-}
+async function  getBoxShadow  (){
+  let shadowVal = boxshadow.style
+  console.log('shadowVal', shadowVal.cssText)
+try {
+  await navigator.clipboard.writeText(shadowVal.cssText);
+  document.getElementById('shadowVal').innerText=shadowVal.cssText
 
-var obj8 ={
-  'wholesss':'wholesss',
- 
+}catch(err){
+
+}
 }
